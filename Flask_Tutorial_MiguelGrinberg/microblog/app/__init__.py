75ed0f226f96d845091c86f9363d277efba226a1
LucaGiona <luca@senzproduction.com>
2025-04-19 23:41:56 +0200

    create: messages.pot - babel.cfg - translations folder

--- Flask_Tutorial_MiguelGrinberg/microblog/app/__init__.py
+++ Flask_Tutorial_MiguelGrinberg/microblog/app/__init__.py
@@ -21,7 +21,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-login.login_message = _l('Please öog in to access this page')
+login.login_message = _l('Please log in to access this page')
 mail = Mail(app)
 moment = Moment(app)
 babel = Babel(app, locale_selector=get_locale)
