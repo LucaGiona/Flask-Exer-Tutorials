856713a5cc2f0c3f1789768bc877b1f8c1f3d157
LucaGiona <luca@senzproduction.com>
2025-04-20 04:59:01 +0200

    client side translate function JS in static folder

---
+++ Flask_Tutorial_MiguelGrinberg/microblog/app/static/js/translate.js
@@ -0,0 +1,18 @@
+async function translate(sourceElem, destElem, sourceLang, destLang){
+    document.getElementById(destElem).innerHTML =
+     '<img src="/static/loading.gif">';
+    
+    const response = await fetch('/translate', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json; charset=utf-8'},
+        body: JSON.stringify({
+            text: document.getElementById(sourceElem).innerText,
+            source_language: sourceLang,
+            dest_language: destLang
+        })
+    });
+
+    const data = await response.json();
+    document.getElementById(destElem).innerText = data.text;
+    
+    }
